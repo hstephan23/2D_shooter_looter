7ce6856bb4ea73b03ace991fe776e1f97053bc06
hstephan23 <harrison@stephanclan.com>
2026-02-06 14:53:08 -0700

    grunts only move when close to the character

--- EngineRoot/Engine/Core/src/monster.cpp
+++ EngineRoot/Engine/Core/src/monster.cpp
@@ -2,26 +2,33 @@
 #include "raylib.h"
 
 #include "monster.h"
+
+#include <cmath>
+
 #include "player.h"
 
 void move_grunt(Monster &grunt, const Player &player, const float delta_time)
 {
-    if (grunt.position.x <= player.position.x)
+    const float dx = player.position.x - grunt.position.x;
+    const float dy = player.position.y - grunt.position.y;
+    if (const float distance = sqrtf(dx * dx + dy * dy); distance <= 250.0f)
     {
-        grunt.position.x += grunt.speed * delta_time;
-    } else if (grunt.position.x >= player.position.x)
-    {
-        grunt.position.x -= grunt.speed * delta_time;
-    }
+        if (grunt.position.x <= player.position.x)
+        {
+            grunt.position.x += grunt.speed * delta_time;
+        } else if (grunt.position.x >= player.position.x)
+        {
+            grunt.position.x -= grunt.speed * delta_time;
+        }
 
-    if (grunt.position.y <= player.position.y)
-    {
-        grunt.position.y += grunt.speed * delta_time;
-    }else if (grunt.position.y >= player.position.y)
-    {
-        grunt.position.y -= grunt.speed * delta_time;
+        if (grunt.position.y <= player.position.y)
+        {
+            grunt.position.y += grunt.speed * delta_time;
+        }else if (grunt.position.y >= player.position.y)
+        {
+            grunt.position.y -= grunt.speed * delta_time;
+        }
     }
-
 }
 
 Monster create_grunt(const Vector2 position)
