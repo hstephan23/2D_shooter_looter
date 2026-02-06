047504ab71c5398f6c7158c11f223e22debc488b
hstephan23 <harrison@stephanclan.com>
2026-02-06 04:09:41 -0700

    Add dash ability to player movement

--- EngineRoot/Engine/Core/src/startup.cpp
+++ EngineRoot/Engine/Core/src/startup.cpp
@@ -11,16 +11,17 @@
 
 void startup()
 {
-    InitWindow(800, 600, "Game_Engine");
+    InitWindow(1000, 800, "Game_Engine");
     constexpr int MAX_BULLETS = 12;
     std::array<Bullet, MAX_BULLETS> bullets = {};
     Player player = {
-        .position = Vector2{400, 300},
+        .position = Vector2{500, 400},
         .health = 100.0f,
         .bullet_speed = 150.0f,
         .bullet_damage = 10.0f,
         .fire_rate = 6.0f,
         .fire_cooldown = 0.0f,
+        .dash_cooldown = 0.0f,
     };
     while (!WindowShouldClose())
     {
@@ -32,19 +33,9 @@ void startup()
 
         const float delta_time = GetFrameTime();
 
-        // This controls auto-fire as well (just hold left-click on the mouse)
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) || IsMouseButtonDown(MOUSE_BUTTON_LEFT))
-        {
-            spawn_bullet(
-               bullets,
-               player,
-               muzzle_position,
-               aim_direction,
-               delta_time
-           );
-        }
-        player_move(player, delta_time);
-
+        spawn_bullet(bullets,player,muzzle_position, aim_direction, delta_time);
+        player_move(player, delta_time, 100.0f);
+        player_dash(player, delta_time);
         update_bullets(bullets, delta_time);
         render(bullets, player.position, gun_position, MAX_BULLETS);
     }
